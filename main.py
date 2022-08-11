53648a03afd6aba351dc103f78eaddeef3b12e16
KismaYT <date20060907@gmail.com>
2022-08-12 00:16:52 +0300

    Создано сохранение, есть баг с созданием одинаковых логинов

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from tkinter import *
 from random import *
 import pyperclip
+import json
 
 
 def Gen():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
                'v', 'w', 'x', 'y', 'z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    symbols = ['!', '#', '$', '&', '(', ')', '*', '+']
+    symbols = ['!', '#', '_', '&', '(', ')', '*', '+']
     password_letters = [choice(letters) for _ in range(randint(2, 4))]
     password_lettersB = [choice(letters).upper() for _ in range(randint(2, 4))]
     password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
@@ -19,8 +20,34 @@ def Gen():
     pyperclip.copy(''.join(password_list))
 
 
+def Coded():
+    a = Code.get()
+    b = 0
+    p = Password.get() + Code.get()
+    r = []
+    for i in a:
+        b += ord(i)
+    for i in p:
+        r.append(chr(ord(i) + b))
+    return ''.join(r)
+
+
 def Save():
-    pass
+    S1 = Website.get()
+    S2 = Login.get()
+    S3 = Coded()
+    with open('password.json', 'r') as file:
+        b = json.load(file)
+        if S1 not in b:
+            b[S1] = []
+        for i in b[S1]:
+            if i[0] == S2:
+                i[1] = S3
+                break
+        else:
+            b[S1] += [S2, S3]
+    with open('password.json', 'w') as file:
+        json.dump(b, file)
 
 
 window = Tk()
@@ -40,9 +67,12 @@ Login.grid(row=2, column=1, columnspan=2)
 Label(text='Password:', width=10, height=2, font=('Arial', 15), anchor='center').grid(row=3, column=0)
 Password = Entry(width=52)
 Password.grid(row=3, column=1, columnspan=1)
+# Кодирование
+Code = Entry(width=52)
+Code.grid(row=4, column=1, columnspan=1)
 # Сохранить
-SaveB = Button(text='Save', width=30, command=Save, bg='ForestGreen')
-SaveB.grid(row=4, column=1)
+SaveB = Button(text='Save', width=15, command=Save, bg='ForestGreen')
+SaveB.grid(row=4, column=2)
 
 # Кнопка генерации
 Generate = Button(text='Generate Password', width=15, command=Gen)
